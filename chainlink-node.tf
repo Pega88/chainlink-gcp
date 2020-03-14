a0b266f2a739cfb17034dfe0247e1ac0016001ff
Niels Buekers <niels.buekers@gmail.com>
2020-03-14 16:53:16 +0100

    ip output

--- chainlink-node.tf
+++ chainlink-node.tf
@@ -181,10 +181,19 @@ resource "kubernetes_service" "chainlink_service" {
   }
 }
 
+resource "google_compute_global_address" "chainlink-node" {
+  name = "chainlink-node"
+}
+
 resource "kubernetes_ingress" "chainlink_ingress" {
   metadata {
     name = "chainlink-ingress"
     namespace = "chainlink"
+    annotations {
+      #"ingress.gcp.kubernetes.io/pre-shared-cert" = "${ var.ssl_cert_name }"
+      #"kubernetes.io/ingress.allow-http" = "false"
+      "kubernetes.io/ingress.global-static-ip-name" = "${ google_compute_global_address.chainlink-node.name }"
+    }
   }
   spec {
     backend {
